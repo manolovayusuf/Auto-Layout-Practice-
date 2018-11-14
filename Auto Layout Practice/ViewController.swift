77df660ad9a1aa3e32aec75f96cf6ac928c0127f
Manny Yusuf <manolovayusuf@pursuit.org>
2018-11-14 14:25:18 -0500

    First commit

---
+++ Auto Layout Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Practice
+//
+//  Created by Manny Yusuf on 11/14/18.
+//  Copyright © 2018 Manny Yusuf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("omg we are doing git")
+    }
+
+
+}
+
